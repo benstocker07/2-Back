59ba6eaecb226c9bc6c122fc27ba53d303838352
benstocker07 <benstocker07@gmail.com>
2025-07-15 00:39:19 +0100

    Update spwf.py

--- spwf.py
+++ spwf.py
@@ -1,9 +1,13 @@
 import hashlib
+
 hash_codes = []
+
 codes = ["23081999", "27072001"]  # add new codes here
+
 for c in codes:
     hashed = hashlib.sha256(c.encode()).hexdigest()
     hash_codes.append(hashed)
+
 login = input('Login Code: ')
     
 login_hash = hashlib.sha256(login.encode()).hexdigest()

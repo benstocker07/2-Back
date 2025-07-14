ac5a77196fa6d7d36834cf425d69dc1982b67cbd
benstocker07 <benstocker07@gmail.com>
2025-07-15 00:40:48 +0100

    Update spwf.py

--- spwf.py
+++ spwf.py
@@ -8,10 +8,19 @@ for c in codes:
     hashed = hashlib.sha256(c.encode()).hexdigest()
     hash_codes.append(hashed)
 
-login = input('Login Code: ')
-    
-login_hash = hashlib.sha256(login.encode()).hexdigest()
-print(f"Login hash: {login_hash}")
+tries = 0 
 
-if login_hash in hash_codes:
-    print("Welcome")
+while True:
+    
+    tries = tries + 1
+
+    login = input('Login Code: ')
+    
+    login_hash = hashlib.sha256(login.encode()).hexdigest()
+    print(f"Login hash: {login_hash}")
+
+    if login_hash in hash_codes:
+        print("Welcome")
+    
+    else:
+        tries = tries + 1

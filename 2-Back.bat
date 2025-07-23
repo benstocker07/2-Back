f53581bcdb3aa20a85fd8601e8d0a20a3043b7a9
benstocker07 <benstocker07@gmail.com>
2025-07-23 15:11:29 +0100

    Update

--- 2-Back.bat
+++ 2-Back.bat
@@ -1,6 +1,6 @@
 @echo off
 
-py -m pyarmor.cli gen Task_Setup.py
-py -m pyarmor.cli gen 2-Back.py
+REM py -m pyarmor.cli gen Task_Setup.py
+REM py -m pyarmor.cli gen 2-Back.py
 python dist/Task_Setup.py
 pause

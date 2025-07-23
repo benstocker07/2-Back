5376f872eaaddf708286c7fad3ebfc13c6a64740
benstocker07 <benstocker07@gmail.com>
2025-07-23 18:20:29 +0100

    Update

---
+++ MongoUpload.py
@@ -0,0 +1,32 @@
+import os
+from pymongo import MongoClient
+import csv
+
+LOCAL_SAVE_PATH = "unsent_data.csv"
+
+def upload_local_csv():
+    if not os.path.exists(LOCAL_SAVE_PATH):
+        print("No local data to upload.")
+        return
+
+    with open(LOCAL_SAVE_PATH, mode="r") as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+
+    if not data:
+        print("CSV is empty.")
+        return
+
+    try:
+        import certifi
+        ca = certifi.where()
+        client = MongoClient("mongodb+srv://2-Back:CTGKXTNQ6SjpGRk7@2-back.yeusf74.mongodb.net/", ssl_cert_reqs=ssl.CERT_NONE)
+        db = client["2-Back"]
+        collection = db["results"]
+        collection.insert_many(data)
+        print("Bulk upload successful. Deleting local file...")
+        os.remove(LOCAL_SAVE_PATH)
+    except Exception as e:
+        print(f"Bulk upload failed: {e}")
+
+upload_local_csv()
--- dist/pyarmor_runtime_000000/__init__.py
+++ dist/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.7 (trial), 000000, 2025-07-23T16:08:03.213967
+# Pyarmor 9.1.7 (trial), 000000, 2025-07-23T18:20:11.152014
 from .pyarmor_runtime import __pyarmor__

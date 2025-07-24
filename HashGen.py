f8d3bf6cd990b709c8b6d13232f6bae1495ac72f
benstocker07 <benstocker07@gmail.com>
2025-07-24 21:29:28 +0100

    Update

--- HashGen.py
+++ HashGen.py
@@ -5,6 +5,5 @@ codes = ["23081999", "27072001"]
 with open("dist/login.txt", "w") as f:
 
     for code in codes:
-
         hash = hashlib.sha256(code.encode()).hexdigest()
         f.write(hash + "\n")

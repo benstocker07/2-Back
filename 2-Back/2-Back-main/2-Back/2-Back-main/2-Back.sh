298301e6e0524a5de0ee6a8911c69601c4573d25
benstocker07 <benstocker07@gmail.com>
2025-11-11 14:23:13 +0000

    Update

--- 2-Back/2-Back-main/2-Back/2-Back-main/2-Back.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 dist/Task_Setup.py
-python3 dist/Upload.py
--- 2-Back/2-Back-main/2-Back/2-Back-main/dist/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.1.7 (trial), 000000, 2025-09-04T21:20:53.100527
-from .pyarmor_runtime import __pyarmor__
